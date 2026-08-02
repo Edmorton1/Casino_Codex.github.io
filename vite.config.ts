6ee17a3b0371c6ea1ff8a05a88318bb9e5782dd6
Edmorton1 <ghettoboy2106@gmail.com>
2026-08-02 21:06:38 +0500

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Casino_Codex.github.io/",
+  base: "/Casino_Codex/",
 })
