2cbd00d6e655afae6eb32292427b75e8af68bae4
Edmorton1 <ghettoboy2106@gmail.com>
2026-08-02 20:35:59 +0500

    remove

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Casino_Codex.github.io/",
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 })
